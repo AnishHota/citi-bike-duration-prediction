2ab4e919591f58974d183184bb12d09cf5f24de6
anishhota <anishhota92@gmail.com>
2024-07-15 12:27:07 -0700

    Training Pipeline: Docker-mage,mlflow

---
+++ training/scripts/start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set PROJECT_NAME=mlops 
+set MAGE_CODE_PATH=/home/src
+set SMTP_EMAIL=$SMTP_EMAIL 
+set SMTP_PASSWORD=$SMTP_PASSWORD 
+docker compose up
---
+++ training/training_mage/transformers/feature_extraction.py
@@ -0,0 +1,49 @@
+import pandas as pd
+
+if 'transformer' not in globals():
+    from mage_ai.data_preparation.decorators import transformer
+if 'test' not in globals():
+    from mage_ai.data_preparation.decorators import test
+
+
+@transformer
+def transform(data, *args, **kwargs):
+    """
+    Template code for a transformer block.
+
+    Add more parameters to this function if this block has multiple parent blocks.
+    There should be one parameter for each output variable from each parent block.
+
+    Args:
+        data: The output from the upstream parent block
+        args: The output from any additional upstream blocks (if applicable)
+
+    Returns:
+        Anything (e.g. data frame, dictionary, array, int, str, etc.)
+    """
+    # Specify your transformation logic here
+
+    train_df = data[0]
+    val_df = data[1]
+
+    categorical_features = [
+        'start_station_id',
+        'end_station_id'
+    ]
+
+    train_df[categorical_features] = train_df[categorical_features].astype(str)
+
+    val_df[categorical_features] = val_df[categorical_features].astype(str)
+
+    train_df["start_end_id"] = train_df["start_station_id"] + "_" + train_df["end_station_id"]
+    val_df["start_end_id"] = val_df["start_station_id"] + "_" + val_df["end_station_id"]
+
+    return [train_df,val_df]
+
+
+@test
+def test_output(output, *args) -> None:
+    """
+    Template code for testing the output of the block.
+    """
+    assert output is not None, 'The output is undefined'
